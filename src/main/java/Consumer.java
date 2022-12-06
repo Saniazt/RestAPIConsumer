8c606283e3807eddad31590c2da573f909b6a749
saniazt <saniazt@gmail.com>
2022-12-06 09:51:09 +0200

    Initial commit

---
+++ src/main/java/Consumer.java
@@ -0,0 +1,25 @@
+import org.springframework.http.HttpEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Consumer {
+    public static void main(String[] args) {
+        RestTemplate restTemplate = new RestTemplate();
+
+
+
+        /*String url = "https://reqres.in/api/users/2"; //for GET
+        String response = restTemplate.getForObject(url,String.class);
+        System.out.println(response);*/
+
+        Map<String,String> jsonToSend = new HashMap<>();//for POST
+        jsonToSend.put("name","Test name");
+        jsonToSend.put("job","Test job");
+        HttpEntity<Map<String,String>> request = new HttpEntity<>(jsonToSend);
+        String url = "https://reqres.in/api/users";
+        String response = restTemplate.postForObject(url,request,String.class);
+        System.out.println(response);
+    }
+}
